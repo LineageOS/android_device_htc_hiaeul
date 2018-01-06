314339150aa2e6e2b6a7b015b02967f86e9a1cc8
Ethan Chen <intervigil@gmail.com>
2018-01-06 15:39:17 -0800

    DO NOT MERGE: Personal GApps setup
    Change-Id: I476201f0cd40e2b9552522e1e0ad4459fcf23cbd

--- lineage.mk
+++ lineage.mk
@@ -3,6 +3,12 @@ $(call inherit-product, device/htc/hiaeul/full_hiaeul.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+GAPPS_VARIANT := stock
+GAPPS_EXCLUDED_PACKAGES := EditorsDocs EditorsSheets EditorsSlides GoogleCamera PrebuiltNewsWeathr
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_FORCE_PIXEL_LAUNCHER := true
+$(call inherit-product,vendor/opengapps/build/opengapps-packages.mk)
+
 PRODUCT_NAME := lineage_hiaeul
 BOARD_VENDOR := htc
 TARGET_VENDOR := htc
